fdbb860fac47c012575d3b08141d9421d2bb9360
Alexander <alexanderlbz@mail.ru>
2024-11-07 19:25:10 +0300

    initial commit 2

---
+++ MospolyJam3/Source/MospolyJam3/MospolyJam3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MospolyJam3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MospolyJam3, "MospolyJam3" );
