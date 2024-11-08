802837b5e4d8a7356d19a08401cd1123a4c499be
Alexander <alexanderlbz@mail.ru>
2024-11-08 15:39:08 +0300

    based on fps

---
+++ MyFPS/Source/MyFPS/MyFPS.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MyFPS : ModuleRules
+{
+	public MyFPS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ MyFPS/Source/MyFPS/MyFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMyFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMyFPSGameMode();
+};
+
+
+
